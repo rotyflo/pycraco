d4d3a27b86606978a592bbbc343bcd663585ff2c
RT Flores <rotyflo@gmail.com>
2018-09-23 23:14:41 -0500

    Rename folders

---
+++ chapter_2/simple_message.py
@@ -0,0 +1,4 @@
+# Store a message in a variable, and then print that message.
+
+your_name = "John Doe"
+print(your_name)
---
+++ chapter_3/names.py
@@ -0,0 +1,5 @@
+my_friends = ["Ron", "Don", "Jon"]
+
+print(my_friends[0])
+print(my_friends[1])
+print(my_friends[2])
