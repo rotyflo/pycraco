05c7d3da3eb945cf6b2653b84f7bf52761e17c2e
RT Flores <rotyflo@gmail.com>
2018-11-01 20:34:55 -0500

    Construct and navigate basic dictionaries

---
+++ try_it_yourself/0601_person.py
@@ -0,0 +1,9 @@
+my_info = {
+    'first_name': 'robert',
+    'last_name': 'flores',
+    'age': 26,
+    'city': 'san antonio'
+}
+
+for key, value in my_info.items():
+    print(key + ": " + str(value))
