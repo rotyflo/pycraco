49bc3baa418b809c078561aa3a8f3fc0a30b12dd
RT <rotyflo@gmail.com>
2019-04-16 13:22:28 -0700

    Keep import explicit throughout the file

--- functions/printing_models.py
+++ functions/printing_models.py
@@ -1,7 +1,7 @@
-import printing_functions as pf
+import printing_functions
 
 unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
 completed_models = []
 
-pf.print_models(unprinted_designs, completed_models)
-pf.show_completed_models(completed_models)
+printing_functions.print_models(unprinted_designs, completed_models)
+printing_functions.show_completed_models(completed_models)
