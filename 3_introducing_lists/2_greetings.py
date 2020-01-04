99eedb92c52f819ce6dfee4402202adaa7f31f80
RT Flores <rotyflo@gmail.com>
2020-01-03 23:01:48 -0800

    Create lists and access their elements

---
+++ 3_introducing_lists/2_greetings.py
@@ -0,0 +1,6 @@
+names = ['john', 'tim', 'steve']
+message = 'How are you doing today, '
+
+print(message + names[0].title() + '?')
+print(message + names[1].title() + '?')
+print(message + names[2].title() + '?')
---
+++ 3_introducing_lists/3_your_own_list.py
@@ -0,0 +1,6 @@
+makes = ['toyota', 'honda', 'mitsubishi']
+message = 'I would like to buy a '
+
+print(message + makes[0].title() + '.')
+print(message + makes[1].title() + '.')
+print(message + makes[2].title() + '.')
