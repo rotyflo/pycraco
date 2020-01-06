4fccb7883625fadeb2470bb96e90bac71ed1f703
RT Flores <rotyflo@gmail.com>
2020-01-05 21:29:14 -0800

    For loop through lists

---
+++ 4_working_with_lists/1_pizzas.py
@@ -0,0 +1,6 @@
+favorite_pizzas = ['pepperoni', 'pineapple', 'cheese']
+
+for pizza in favorite_pizzas:
+    print('I like ' + pizza + ' pizza')
+
+print('I really love pizza!')
---
+++ 4_working_with_lists/2_animals.py
@@ -0,0 +1,6 @@
+similar_animals = ['tiger', 'lion', 'panther']
+
+for animal in similar_animals:
+    print('A ' + animal + ' would make a great pet')
+
+print('Any of these animals would make a great pet!')
